1b8915941084ea4893324b997cc18acfe5a43450
abda-torey <abdatorey@gmail.com>
2025-04-23 14:48:44 +0000

    flink jib to move newly created api source data from gcs to bq

---
+++ dags/flinkjob_to_gcs/from_api_r030_gcs.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+}
+
+dag = DAG(
+    'api_r030_to_gcs',
+    default_args=default_args,
+    description='Move driving test data from api to GCS using Flink job',
+    schedule_interval=None,
+    start_date=days_ago(1),
+    tags=['flink_job'],
+)
+
+run_flink_command = """
+docker exec -it flink-jobmanager ./bin/flink run -py /opt/src/job/ingest_ro30_api_gcs.py
+"""
+
+run_flink_api_data_task = BashOperator(
+    task_id='run_r030_api_flink_job',
+    bash_command=run_flink_command,
+    dag=dag,
+)
+
+run_flink_api_data_task
